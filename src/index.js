2c401c03d768d089af6225d250774ca17b5a34a8
chinmay <2vd23ci401@klsvdit.edu.in>
2026-08-14 17:43:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express server" });
+});
+
+const PORT = 8000;
+const url = `http://localhost:${PORT}`;
+
+app.listen(PORT, () => {
+  console.log(`Server started. URL: ${url}`);
+});
