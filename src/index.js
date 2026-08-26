ecc1b861eb4fab32117b5283cea439669a9de375
chinmay <2vd23ci401@klsvdit.edu.in>
2026-08-26 16:09:10 +0530

    Add matches endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
+const PORT = 8000;
+const url = `http://localhost:${PORT}`;
 
 // JSON middleware
 app.use(express.json());
@@ -10,9 +13,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from Express server" });
 });
 
-const PORT = 8000;
-const url = `http://localhost:${PORT}`;
-
+app.use('/matches', matchRouter)
 app.listen(PORT, () => {
   console.log(`Server started. URL: ${url}`);
 });
