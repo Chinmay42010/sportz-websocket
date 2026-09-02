a16d33b3aab7c7d40c35f6318a0be365050a68ce
chinmay <2vd23ci401@klsvdit.edu.in>
2026-09-02 13:22:31 +0530

    Minor bug fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,35 +1,50 @@
 import { WebSocket, WebSocketServer } from "ws";
 
 function sendJson(socket, payload) {
-    if(socket.readyState !== WebSocket.OPEN) return;
+  if (socket.readyState !== WebSocket.OPEN) return;
 
-    socket.send(JSON.stringify(payload))
+  socket.send(JSON.stringify(payload));
 }
 
 function broadcast(wss, payload) {
-    for (const client of wss.clients) {
-        if (client.readyState !== WebSocket.OPEN) return;
+  for (const client of wss.clients) {
+    if (client.readyState !== WebSocket.OPEN) continue;
 
-        client.send(JSON.stringify(payload));
-    }
+    client.send(JSON.stringify(payload));
+  }
 }
 
 export function attachWebSocketServer(server) {
-    const wss = new WebSocketServer({
-        server,
-        path: "/ws",
-        maxPayload: 1024 * 1024,
-    })
+  const wss = new WebSocketServer({
+    server,
+    path: "/ws",
+    maxPayload: 1024 * 1024,
+  });
 
-    wss.on("connection", (socket) =>{
-        sendJson(socket, { type: "welcome" });
+  wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
 
-        socket.on("error", console.error);
-    })
+    sendJson(socket, { type: "welcome" });
 
-    function broadcastMatchCreated(match) {
-        broadcast(wss, { type: "match_created", data: match});
-    }
+    socket.on("error", console.error);
+  });
 
-    return { broadcastMatchCreated }
-} 
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+      ws.isAlive = false;
+      ws.ping();
+    }, 30000);
+
+    wss.on("close", () => clearInterval(interval));
+  });
+
+  function broadcastMatchCreated(match) {
+    broadcast(wss, { type: "match_created", data: match });
+  }
+
+  return { broadcastMatchCreated };
+}
